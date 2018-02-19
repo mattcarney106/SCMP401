1d6ce0c88ae7b762f4dc94624c83ccdf53da391e
Matthew Carney <mattcarney106@gmail.com>
2018-02-19 11:50:37 -0500

    Added function to compute BEL rate from power output.

--- include/solar.h
+++ include/solar.h
@@ -1,3 +1,4 @@
 /* Functions used for solar panel data analysis */
 
 void helloWorld();
+double CalcBELRate(double powerOutput);
--- src/solar.cpp
+++ src/solar.cpp
@@ -13,3 +13,24 @@ void helloWorld()
 {
   cout << "Hello, world!\n";
 }
+
+/* Takes in powerOutput and returns
+   The BEL rate in USD per kWhr
+*/
+double CalcBELRate(double powerOutput)
+{
+  double rate;
+  if(powerOutput >= 0.0 && powerOutput < 51.0){
+    rate = 0.30;
+  }else if(powerOutput >= 51.0 && powerOutput < 201.0){
+    rate = 0.36;
+  }else if(powerOutput >= 201.0){
+    rate = 0.40;
+  }else{
+    cout << "INVALID POWER OUTPUT\n";
+  }
+
+  return rate;
+}
+
+double 
